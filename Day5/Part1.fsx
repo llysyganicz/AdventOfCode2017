07c27d930549c679a05449a12f3c03aa57450abb
Łukasz Łysyganicz <lysy01@gmail.com>
2017-12-05 17:30:16 +0100

    Day 5 part 1

---
+++ Day5/Part1.fsx
@@ -0,0 +1,18 @@
+#!/usr/bin/fsharpi
+
+open System.IO
+
+let doInstructions (instructions: int[]) =
+    let length = instructions.Length
+    let rec jump (index, steps) =
+        let jumpLength = instructions.[index]
+        let newIndex = index + jumpLength
+        instructions.[index] <- jumpLength + 1
+        if newIndex > length - 1 || newIndex < 0 then steps + 1
+        else jump(newIndex, steps + 1)
+
+    jump(0, 0)
+
+let instructions = File.ReadLines "input.txt" |> Seq.map(fun e -> int(e)) |> Seq.toArray
+let result = doInstructions instructions
+printfn "%d" result
