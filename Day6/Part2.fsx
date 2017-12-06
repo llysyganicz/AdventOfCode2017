f471536a0bcfc173c2001fb09e35dc70b6b0d199
Łukasz Łysyganicz <lysy01@gmail.com>
2017-12-06 19:02:54 +0100

    Day 6 part 2

---
+++ Day6/Part2.fsx
@@ -0,0 +1,23 @@
+#!/usr/bin/fsharpi
+
+open System.IO
+open System.Linq
+
+let findLoop (banks: int[]) =
+    let rec step states =
+        let max = banks |> Array.max
+        let maxIndex = banks |> Array.findIndex(fun e -> e = max)
+        banks.[maxIndex] <- 0
+        for i = 1 to max do
+            let index = (maxIndex + i) % banks.Length
+            banks.[index] <- banks.[index] + 1
+        if states |> List.exists(fun e -> Enumerable.SequenceEqual(e, banks)) then 
+            let index = states |> List.findIndex(fun e -> Enumerable.SequenceEqual(e, banks))
+            states.Length - index
+        else step (states @ [(Array.toList banks)])
+    
+    step [(Array.toList banks)]
+
+let banks = (File.ReadAllText "input.txt").Split [| '\t' |] |> Array.map(fun e -> int(e))
+let result = findLoop banks
+printfn "%d" result
